c671afc1ad20189e88db5316d6d917569b44f8c1
Darren W <darrenwallace@gmail.com>
2024-12-09 17:16:40 -0500

    5 second loop for testing

--- src/servo_pkg/servo_pkg/servo_client.py
+++ src/servo_pkg/servo_pkg/servo_client.py
@@ -1,14 +1,15 @@
 import rclpy
+from rclpy import time
 from rclpy.node import Node
 from interfaces.srv import MoveServo
 
-import time
-
+import random
 
 class Servo_Client(Node):
     def __init__(self):
         super().__init__("servo_Client")
-
+        
+        self.timer = self.create_timer(5.0, self.servo_tester)
         self.cli = self.create_client(MoveServo, "servo_service")
         while not self.cli.wait_for_service(timeout_sec=1.0):
             self.get_logger().info("service not available, waiting again...")
@@ -26,15 +27,21 @@ class Servo_Client(Node):
         Servo_Client.get_logger(self).info(
             "Results: %s, status: %s" % (response.status, response.status_msg)
         )
+        
+    def servo_tester(self) -> None:
+        random_pos_usb = random.randint(500, 2400)
+        random_pos_i2c = random.randint(0, 180)
+        
+        self.servo_request(self, 0, random_pos_usb)
+        
+        self.servo_request(self, 0, random_pos_i2c)
+
+        
 
 def main(args=None):
     rclpy.init(args=args)
     servo_Client = Servo_Client()
-
-    Servo_Client.servo_request(servo_Client, 0, 500)
-    time.sleep(2)
-    Servo_Client.servo_request(servo_Client, 0, 2400)
-
+    rclpy.spin(servo_Client)
     servo_Client.destroy_node()
     rclpy.shutdown()
 
