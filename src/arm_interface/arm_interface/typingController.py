2d0a43ad474f720caa7f4623e0cc0559215c9682
MaybeWilli <51888361+MaybeWilli@users.noreply.github.com>
2025-01-11 13:25:13 -0500

    Reformateed

--- src/arm_interface/arm_interface/typingController.py
+++ src/arm_interface/arm_interface/typingController.py
@@ -13,20 +13,26 @@ from trajectory_msgs.msg import JointTrajectoryPoint
 from math import pi
 
 from moveit_msgs.action import ExecuteTrajectory
-#import moveit_msgs.action
+
+# import moveit_msgs.action
 
 
 class typingController(Node):
     def __init__(self):
         super().__init__("trajectoryInt")
-        
+
         self.get_logger().info("Running node")
-        
+
         self.traj_sub = self.create_subscription(
-            ExecuteTrajectory.FeedbackMessage(), "/execute_trajectory/_action/feedback", self.traj_callback, 10) 
-    
+            ExecuteTrajectory.FeedbackMessage(),
+            "/execute_trajectory/_action/feedback",
+            self.traj_callback,
+            10,
+        )
+
     def traj_callback(self, msg):
         self.get_logger().info(f"Message: {msg}")
+
 
 def main(args=None):
     rclpy.init(args=args)
@@ -34,6 +40,7 @@ def main(args=None):
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
-    
+
+
 if __name__ == "__main__":
     main()
--- src/arm_interface/setup.py
+++ src/arm_interface/setup.py
@@ -1,27 +1,26 @@
 from setuptools import find_packages, setup
 
-package_name = 'arm_interface'
+package_name = "arm_interface"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='will',
-    maintainer_email='51888361+MaybeWilli@users.noreply.github.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="will",
+    maintainer_email="51888361+MaybeWilli@users.noreply.github.com",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-          'trajectory_interpreter = arm_interface.trajectoryInterpreter:main',
-          'typing_controller = arm_interface.typingController:main'
+        "console_scripts": [
+            "trajectory_interpreter = arm_interface.trajectoryInterpreter:main",
+            "typing_controller = arm_interface.typingController:main",
         ],
     },
 )
