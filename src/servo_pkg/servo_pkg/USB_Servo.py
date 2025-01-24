1004a2f07e562b0f3bcc4babfb8c426a4421e8dc
Darren W <darrenwallace@gmail.com>
2025-01-24 18:09:27 -0500

    formatting

--- src/servo_pkg/servo_pkg/USB_Servo.py
+++ src/servo_pkg/servo_pkg/USB_Servo.py
@@ -12,11 +12,11 @@ class USB_Servo(Node):
         super().__init__("usb_servo")
 
         self.declare_parameter(
-            "serial_port", '/dev/ttyACM0'
+            "serial_port", "/dev/ttyACM0"
         )  # default to '/dev/ttyACM0' port
         self.servo = maestro.Controller(
-            #self.get_parameter("serial_port").get_parameter_value().string_value
-            '/dev/ttyACM0'
+            # self.get_parameter("serial_port").get_parameter_value().string_value
+            "/dev/ttyACM0"
         )
 
         self.srv = self.create_service(MoveServo, "servo_service", self.set_position)
@@ -24,7 +24,7 @@ class USB_Servo(Node):
         # These ranges come from the tower pro mini 9g data sheet
         # 512 - 2400 microseconds, postions must be in quarter-microseoncds, so 2048 - 9600
         # https://www.friendlywire.com/projects/ne555-servo-safe/SG90-datasheet.pdf
-        # 
+        #
         # For the HS-40 615-2495 microseconds, converted: 2460 - 9980
         # https://www.servocity.com/hs-40-servo/
         self.min = 2048  # 0 Degrees
@@ -50,15 +50,19 @@ class USB_Servo(Node):
             self.max = self.convert_from_degrees(request.max)
 
         self.servo.setRange(request.port, self.min, self.max)
-        if self.convert_from_degrees(request.pos) > self.max or self.convert_from_degrees(request.pos) < self.min:
+        if (
+            self.convert_from_degrees(request.pos) > self.max
+            or self.convert_from_degrees(request.pos) < self.min
+        ):
             response.status = False
             current_position = self.servo.getPosition(request.port)
             response.status_msg = f"Servo {request.port} input out of range\ncurrent position: {current_position}"
 
         else:
             USB_Servo.get_logger(self).info(
-            "Received request for: %s, Converted: %s" % (request.pos, self.convert_from_degrees(request.pos))
-        )
+                "Received request for: %s, Converted: %s"
+                % (request.pos, self.convert_from_degrees(request.pos))
+            )
             self.servo.setTarget(request.port, self.convert_from_degrees(request.pos))
 
             response.status = True
@@ -77,9 +81,10 @@ class USB_Servo(Node):
         return response
 
     def convert_from_degrees(self, degrees: int) -> int:
-        return int(self.ZERO_DEGREES_VALUE + (
-            self.CONVERSION_VALUE / (self.MAX_DEGREES / degrees)
-        ))
+        return int(
+            self.ZERO_DEGREES_VALUE
+            + (self.CONVERSION_VALUE / (self.MAX_DEGREES / degrees))
+        )
 
 
 def main(args=None):
