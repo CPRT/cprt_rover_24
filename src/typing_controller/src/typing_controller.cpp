9364df396bfb02fefb56b50fbb0bd1ef484da050
MaybeWilli <51888361+MaybeWilli@users.noreply.github.com>
2025-02-04 16:54:21 -0500

    Autonomous typing nodes

--- src/typing_controller/src/typing_controller.cpp
+++ src/typing_controller/src/typing_controller.cpp
@@ -4,11 +4,33 @@ TypingNode::TypingNode() : Node("Typing_controller")
 {
   subscription_ = this->create_subscription<std_msgs::msg::String>("typing_topic", 10, std::bind(&TypingNode::topic_callback, this, std::placeholders::_1));
   subscription2_ = this->create_subscription<moveit_msgs::action::ExecuteTrajectory_FeedbackMessage>("execute_trajectory/_action/feedback", 10, std::bind(&TypingNode::arm_callback, this, std::placeholders::_1));
+  
+  //tf client
+  node = rclcpp::Node::make_shared("get_angle_client");
+  client = node->create_client<interfaces::srv::KeycapCmd>("keycap_cmd");
+  
+  while (!client->wait_for_service(1s)) {
+    if (!rclcpp::ok()) {
+      RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Interrupted while waiting for the service. Exiting.");
+      return;
+    }
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "service not available, waiting again...");
+  }
 }
 
 void TypingNode::topic_callback(const std_msgs::msg::String &msg)
 {
   RCLCPP_INFO(this->get_logger(), "I got %s", msg.data.c_str());
+  auto request = std::make_shared<interfaces::srv::KeycapCmd::Request>();
+    request->keys = false;
+    auto result = client->async_send_request(request);
+    // Wait for the result.
+    if (rclcpp::spin_until_future_complete(node, result) ==
+      rclcpp::FutureReturnCode::SUCCESS)
+    {
+      auto resultCopy = result.get();
+      RCLCPP_INFO(this->get_logger(), "Centimeter: %f, %f, %f", resultCopy->x, resultCopy->y, resultCopy->z);
+    }
 }
 
 void TypingNode::arm_callback(const moveit_msgs::action::ExecuteTrajectory_FeedbackMessage &msg)
--- src/typing_controller/src/typing_controller.h
+++ src/typing_controller/src/typing_controller.h
@@ -11,12 +11,15 @@
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 #include "interfaces/msg/arm_cmd.hpp"
+#include "interfaces/srv/keycap_cmd.hpp"
 #include <moveit/robot_state/robot_state.h>
 //#include <moveit/planning_scene/planning_scene.h>
 //#include <moveit/planning_scene_monitor/planning_scene_monitor.h>
 //#include <moveit/motion_planning_rviz_plugin/motion_planning_display.h>
 
 bool isEmpty(const geometry_msgs::msg::Pose &p);
+
+using namespace std::chrono_literals;
 
 class TypingNode : public rclcpp::Node
 {
@@ -28,7 +31,10 @@ class TypingNode : public rclcpp::Node
     rclcpp::Subscription<moveit_msgs::action::ExecuteTrajectory_FeedbackMessage>::SharedPtr subscription2_;
     void topic_callback(const std_msgs::msg::String &msg);
     void arm_callback(const moveit_msgs::action::ExecuteTrajectory_FeedbackMessage &msg);
-    //moveit_rviz_plugin::MotionPlanningDisplay motionPlanningDisplay;
+    
+    //things for tf
+    std::shared_ptr<rclcpp::Node> node;
+    rclcpp::Client<interfaces::srv::KeycapCmd>::SharedPtr client;
 };
 
 #endif
