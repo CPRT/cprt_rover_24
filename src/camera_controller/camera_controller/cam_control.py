2316630408ca9c42e6938c4ab7bf7c3474e8ad8b
ConnorN <connor.needham2015@gmail.com>
2025-02-07 09:36:21 -0500

    Connor refactor

--- src/camera_controller/camera_controller/cam_control.py
+++ src/camera_controller/camera_controller/cam_control.py
@@ -5,36 +5,74 @@ from rclpy.node import Node
 from interfaces.srv import MoveServo
 from sensor_msgs.msg import Joy
 
-#import random
 
 class Cam_Servo_Client(Node):
-    #Ports that the servos are in
-    PORT_X = 0
-    PORT_Y = 1
-
-    #Bounds for the signal the controller sends
-    MIN_INPUT = 0
-    MAX_INPUT = 180
-    MID_INPUT = int((MAX_INPUT - MIN_INPUT) / 2 + MIN_INPUT)
-
-    #Bounds for the mobility of the servos
-    MIN_SERVO = 0
-    MAX_SERVO = 180
-    MID_SERVO = int((MAX_SERVO - MIN_SERVO) / 2 + MIN_SERVO)
-
-    #Miscellaneous
-    MOD = 9
-    last_pos_x = 0
-    last_pos_y = 0
 
     def __init__(self):
-        super().__init__("cam_servo_Client")
-
+        super().__init__("cam_servo_client")
         self.cli = self.create_client(MoveServo, "cam_servo_service")
         while not self.cli.wait_for_service(timeout_sec=1.0):
-            self.get_logger().info("service not available, waiting again...")
-        self.timer = self.create_timer(2, self.camera_mover)
-        self.subscription = self.create_subscription(
+            self.get_logger().warn("service not available, waiting again...")
+        self.declare_parameters()
+        self.goal_pos_x = self.default_pos
+        self.goal_pos_y = self.default_pos
+        self.last_pos_x = self.default_pos
+        self.last_pos_y = self.default_pos
+        self.ready = True
+        period = (
+            1.0 / self.get_parameter("frequency").get_parameter_value().double_value
+        )
+        self.timer = self.create_timer(period, self.camera_mover)
+        self.subscription = self.create_subscription(Joy, "joy", self.joy_callback, 10)
+
+    def declare_parameters():
+        self.declare_parameter("port_x", 0)
+        self.declare_parameter("port_y", 1)
+        self.declare_parameter("min_servo", 0)
+        self.declare_parameter("max_servo", 180)
+        self.declare_parameter("default_pos", 90)
+        self.declare_parameter("step_size", 2)
+        self.declare_parameter("x_axis_index", 1)
+        self.declare_parameter("y_axis_index", 0)
+        self.declare_parameter("default_button", -1)
+        self.declare_parameter("frequency", 10.0)
+        self.port_x = self.get_parameter("port_x").get_parameter_value().integer_value
+        self.port_y = self.get_parameter("port_y").get_parameter_value().integer_value
+        self.min_servo = (
+            self.get_parameter("min_servo").get_parameter_value().integer_value
+        )
+        self.max_servo = (
+            self.get_parameter("max_servo").get_parameter_value().integer_value
+        )
+        self.default_pos = (
+            self.get_parameter("default_pos").get_parameter_value().integer_value
+        )
+        self.step_size = (
+            self.get_parameter("step_size").get_parameter_value().integer_value
+        )
+        self.x_axis_index = (
+            self.get_parameter("x_axis_index").get_parameter_value().integer_value
+        )
+        self.y_axis_index = (
+            self.get_parameter("y_axis_index").get_parameter_value().integer_value
+        )
+        self.default_button = (
+            self.get_parameter("default_button").get_parameter_value().integer_value
+        )
+
+    def joy_callback(self, msg: Joy) -> None:
+        if self.default_button != -1:
+            if msg.buttons[self.default_button] == 1:
+                self.goal_pos_x = self.default_pos
+                self.goal_pos_y = self.default_pos
+                return
+        if not self.ready:
+            return
+        self.goal_pos_x = self.last_pos_x + self.step_size * msg.axes[self.x_axis_index]
+        self.goal_pos_y = self.last_pos_y + self.step_size * msg.axes[self.y_axis_index]
+        self.goal_pos_x = max(self.min_servo, min(self.max_servo, self.goal_pos_x))
+        self.goal_pos_y = max(self.min_servo, min(self.max_servo, self.goal_pos_y))
+        self.ready = False
 
     def send_request(self, port: int, pos: int, min: int, max: int) -> MoveServo:
         req = MoveServo.Request()
@@ -44,38 +82,18 @@ class Cam_Servo_Client(Node):
         req.max = max
         future = self.cli.call_async(req)
 
-    def servo_request(self, req_port, req_pos, req_min, req_max) -> None:
-        Cam_Servo_Client.get_logger(self).info("Sending Request for: %s" % (req_pos))
-        self.send_request(port=req_port, pos=req_pos, min=req_min, max=req_max)
-
-    def convert_command(self, input_signal) -> int: #Turns the signal into an increment to be added to the most recent position
-        input_signal = input_signal - self.MID_INPUT
-        input_signal %= self.MOD
-
-        return input_signal
-                
-    def bound_enforce(self, servo_command) -> int: #Makes sure the servo command doesnt go out of bounds
-        if(servo_command < self.MIN_SERVO):
-            servo_command = self.MIN_SERVO
-        elif(servo_command > self.MAX_SERVO):
-            servo_command = self.MAX_SERVO
-
-        return servo_command
-
-    def camera_mover(self) -> None: #I was told the joystick sends an angle between 0 and 180
-
-        input_signal_x = 0
-        input_signal_y = 0
-
-        #Here would go the code to fetch the input, if I knew what node was giving the input
-
-        input_signal_x = self.bound_enforce( self.last_pos_x + self.convert_command(self, input_signal_x) )
-        input_signal_y = self.bound_enforce( self.last_pos_y + self.convert_command(self, input_signal_y) )
-
-        self.servo_request(0, input_signal_x, 0, 180)
-        self.last_pos_x = input_signal_x
-        self.servo_request(0, input_signal_y, 0, 180)
-        self.last_pos_y = input_signal_y
+    def camera_mover(self) -> None:
+        if self.goal_pos_x != self.last_pos_x:
+            self.send_request(
+                self.port_x, self.goal_pos_x, self.min_servo, self.max_servo
+            )
+            self.last_pos_x = self.goal_pos_x
+        if self.goal_pos_y != self.last_pos_y:
+            self.send_request(
+                self.port_y, self.goal_pos_y, self.min_servo, self.max_servo
+            )
+            self.last_pos_y = self.goal_pos_y
+        self.ready = True
 
 
 def main(args=None):
---
+++ src/camera_controller/launch/camera_controller.launch.py
@@ -0,0 +1,26 @@
+import launch
+import launch_ros.actions
+
+
+def generate_launch_description():
+    return launch.LaunchDescription(
+        [
+            launch_ros.actions.Node(
+                package="camera_controller",
+                executable="cam_control",
+                name="gas_sensor",
+                parameters=[
+                    {"port_x": 0},
+                    {"port_y": 1},
+                    {"min_servo": 0},
+                    {"max_servo": 180},
+                    {"default_pos": 90},
+                    {"step_size": 1},
+                    {"x_axis_index": 1},
+                    {"y_axis_index": 0},
+                    {"default_button": -1},
+                    {"frequency": 10.0},
+                ],
+            ),
+        ]
+    )
