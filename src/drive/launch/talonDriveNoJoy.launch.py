13499200caf6e8cfad00e206f6ef3a5cfed69923
Chris <christopherrusu1@gmail.com>
2025-02-24 20:56:12 -0500

    yolo committing talon drive without a joy node

---
+++ src/drive/launch/talonDriveNoJoy.launch.py
@@ -0,0 +1,104 @@
+import launch
+import launch_ros.actions
+from launch_ros.actions import ComposableNodeContainer
+from launch_ros.descriptions import ComposableNode
+
+P = 2.00
+I = 0.000002
+D = 0.0000000001
+
+
+def generate_launch_description():
+    """Generate launch description with multiple components."""
+
+    container = ComposableNodeContainer(
+        name="PhoenixContainer",
+        namespace="",
+        package="ros_phoenix",
+        executable="phoenix_container",
+        parameters=[{"interface": "can0"}],
+        composable_node_descriptions=[
+            ComposableNode(
+                package="ros_phoenix",
+                plugin="ros_phoenix::TalonSRX",
+                name="frontLeft",
+                parameters=[
+                    {"id": 1},
+                    {"P": P},
+                    {"I": I},
+                    {"D": D},
+                    {"max_voltage": 24.0},
+                    {"brake_mode": True},
+                ],
+            ),
+            ComposableNode(
+                package="ros_phoenix",
+                plugin="ros_phoenix::TalonSRX",
+                name="backLeft",
+                parameters=[
+                    {"id": 2},
+                    {"P": P},
+                    {"I": I},
+                    {"D": D},
+                    {"max_voltage": 24.0},
+                    {"brake_mode": True},
+                ],
+            ),
+            ComposableNode(
+                package="ros_phoenix",
+                plugin="ros_phoenix::TalonSRX",
+                name="frontRight",
+                parameters=[
+                    {"id": 3},
+                    {"P": P},
+                    {"I": I},
+                    {"D": D},
+                    {"max_voltage": 24.0},
+                    {"brake_mode": True},
+                ],
+            ),
+            ComposableNode(
+                package="ros_phoenix",
+                plugin="ros_phoenix::TalonSRX",
+                name="backRight",
+                parameters=[
+                    {"id": 4},
+                    {"P": P},
+                    {"I": I},
+                    {"D": D},
+                    {"max_voltage": 24.0},
+                    {"brake_mode": True},
+                ],
+            ),
+        ],
+        output="screen",
+    )
+
+    return launch.LaunchDescription(
+        [
+            container,
+            launch_ros.actions.Node(
+                package="drive_cpp",
+                executable="drive_cpp",
+                name="talon_control_node",
+                parameters=[
+                    {"wheels": ["frontRight", "frontLeft", "backRight", "backLeft"]},
+                    {"max_speed": 2.0},
+                    {"base_width": 0.9},
+                    {"pub_odom": True},
+                    {"pub_elec": True},
+                    {"wheel_rad": 0.10},
+                ],
+            ),
+            launch_ros.actions.Node(
+                package="drive",
+                executable="joystick_controller",
+                name="joystick_controller",
+                parameters=[
+                    {"linear_axis_index": 1},
+                    {"turn_axis_index": 0},
+                    {"max_linear_speed": 2.0},
+                ],
+            ),
+        ]
+    )
