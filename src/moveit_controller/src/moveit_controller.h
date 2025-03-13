4acc1f0badc145d1900809baa7608baec05059d6
MaybeWilli <51888361+MaybeWilli@users.noreply.github.com>
2025-03-13 16:11:39 -0400

    Fixed unintended change

--- src/moveit_controller/src/moveit_controller.h
+++ src/moveit_controller/src/moveit_controller.h
@@ -1,6 +1,11 @@
 #ifndef MOVEIT_CONTROLLER_H
 #define MOVEIT_CONTROLLER_H
 
+// TODO: Set to best position to drive in
+#define ARM_DEFAULT_X 0.636922
+#define ARM_DEFAULT_Y 0.064768
+#define ARM_DEFAULT_Z 0.678810
+
 #include <moveit/move_group_interface/move_group_interface.h>
 #include <moveit/robot_state/robot_state.h>
 #include <tf2/LinearMath/Quaternion.h>
@@ -13,43 +18,30 @@
 
 #include "interfaces/msg/arm_cmd.hpp"
 #include "std_msgs/msg/string.hpp"
-//#include <moveit/planning_scene/planning_scene.h>
-//#include <moveit/planning_scene_monitor/planning_scene_monitor.h>
-//#include <moveit/motion_planning_rviz_plugin/motion_planning_display.h>
 
 void executeTrajectory(moveit_msgs::msg::RobotTrajectory &traj,
                        moveit::planning_interface::MoveGroupInterfacePtr mgi);
 
-void executePlan(
-    moveit::planning_interface::MoveGroupInterface::Plan &rotationPlan,
-    moveit::planning_interface::MoveGroupInterfacePtr mgi);
-
 bool isEmpty(const geometry_msgs::msg::Pose &p);
 
-class TestNode : public rclcpp::Node {
+class MoveitController : public rclcpp::Node {
  public:
-  TestNode(const rclcpp::NodeOptions &options);
+  MoveitController(const rclcpp::NodeOptions &options);
 
  private:
+  static constexpr double JUMP_THRESHOLD = 0;
+  static constexpr double EEF_STEP = 0.01;
   rclcpp::Subscription<interfaces::msg::ArmCmd>::SharedPtr subscription_;
 
-  std::string node_name;
-  moveit::planning_interface::MoveGroupInterfacePtr move_group_ptr;
-  rclcpp::Node::SharedPtr node_ptr;
-  rclcpp::Executor::SharedPtr executor_ptr;
-  std::thread executor_thread;
-  std::thread th;
-  moveit_msgs::msg::RobotTrajectory trajectory;
-
-  moveit::planning_interface::MoveGroupInterface::Plan rotationPlan;
+  moveit::planning_interface::MoveGroupInterfacePtr move_group_ptr_;
+  rclcpp::Node::SharedPtr node_ptr_;
+  rclcpp::Executor::SharedPtr executor_ptr_;
+  std::thread executor_thread_;
+  std::thread move_it_thread_;
+  moveit_msgs::msg::RobotTrajectory trajectory_;
+  geometry_msgs::msg::Pose default_pose_;
 
   void topic_callback(const interfaces::msg::ArmCmd &armMsg);
-
-  rclcpp::Publisher<trajectory_msgs::msg::JointTrajectory>::SharedPtr
-      publisher_;
-  moveit::core::RobotStatePtr robotStatePtr;
-  const double num = 0;
-  // moveit_rviz_plugin::MotionPlanningDisplay motionPlanningDisplay;
 };
 
 #endif
