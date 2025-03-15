4f145671fb8eba33cacccd00a13e42699efbbd8d
DeclanMcIntaggart <Declan.McIntaggart@gmail.com>
2025-03-15 12:46:13 -0400

    Added launch file and matched naming convention

---
+++ src/localization/launch/imu.launch.py
@@ -0,0 +1,20 @@
+import launch
+import launch_ros.actions
+
+
+def generate_launch_description():
+    return launch.LaunchDescription(
+        [
+            launch_ros.actions.Node(
+                package="localization",
+                executable="imu_pub_node",
+                name="localization_node",
+                parameters=[
+                    {"orientation_covariance":[0.02, 0, 0, 0, 0.02, 0, 0, 0, 0.02]},  #orientation_covariance
+                    {"angular_velocity_covariance":[0.01, 0, 0, 0, 0.01, 0, 0, 0, 0.01]},  #angular_velocity_covariance
+                    {"linear_acceleration_covariance":[0.05, 0, 0, 0, 0.05, 0, 0, 0, 0.05]},  #linear_acceleration_covariance
+                    {"Freq": 2.0},  # Publish rate (hz)
+                ],
+            ),
+        ]
+    )
