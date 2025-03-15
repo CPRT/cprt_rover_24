dab3e2b75457ebcd710e3a9028d547a3b45b1c71
ConnorN <connor.needham2015@gmail.com>
2025-03-15 11:25:13 -0400

    Modualize first time install

---
+++ setup/firstTimeInstall.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+SCRIPT_DIR="$(dirname "$0")"
+CURRENT_DIR="$(pwd)"
+
+if [ "$SCRIPT_DIR" == "$CURRENT_DIR" ] || [ "$SCRIPT_DIR" == "." ]; then
+    echo "The script is being run from its own directory."
+else
+    echo "The script is NOT being run from its own directory."
+    exit 1
+fi
+
+./install_ros_humble.sh
+source ~/.bashrc
+./install_gstreamer.sh
+source ~/.bashrc
+./install_nav_deps.sh
+source ~/.bashrc
+./setup_service.sh
