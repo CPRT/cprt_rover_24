06892b51b0a916a0321f2f55724f35b1893039b3
UserC2 <85082371+UserC2@users.noreply.github.com>
2025-03-26 20:18:57 -0400

    wip

--- firstTimeInstall.sh
+++ firstTimeInstall.sh
@@ -2,7 +2,7 @@
 
 locale  # check for UTF-8
 
-set -e
+# set -e
 
 sudo apt update && sudo apt install -y locales
 sudo locale-gen en_US en_US.UTF-8
@@ -14,10 +14,10 @@ locale  # verify settings
 sudo apt install -y software-properties-common
 sudo add-apt-repository -y universe
 
-sudo apt update && sudo apt install curl -y
-sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+# sudo apt update && sudo apt install curl -y
+# sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+# echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
 sudo apt update
 
@@ -128,6 +128,8 @@ echo "export GST_PLUGIN_PATH=$GST_PLUGIN_PATH" >> $GSTREAMER_DIR/setupGstreamer.
 echo "Finished building GStreamer"
 
 source ~/.bashrc
+
+sudo apt install zstd
 
 echo "Downloading ZED SDK ..."
 cd /tmp
---
+++ vision.md
@@ -0,0 +1,5 @@
+* can either train your own model (with training data from previous model)
+* or convert other model
+
+* preferred to use onnx machine learning model (for compatibility with zed camera ros detection)
+* tensorflow getting started doc on #software-general
