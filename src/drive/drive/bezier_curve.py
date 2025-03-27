714f27739055d4ceb4cd34fdb9c5f353e01e819c
Triet Duong <triet.m.duong@gmail.com>
2025-03-26 21:50:56 -0400

    Black cleanup

--- src/drive/drive/bezier_curve.py
+++ src/drive/drive/bezier_curve.py
@@ -1,8 +1,10 @@
 import numpy as np
 from scipy.special import comb
 
+
 def bernstein_poly(i, n, t):
-    return comb(n, i) * ( t**(n - i) ) * (1 - t)**i
+    return comb(n, i) * (t ** (n - i)) * (1 - t) ** i
+
 
 def bezier_curve(points, nTimes=1000):
     nPoints = len(points)
@@ -11,24 +13,23 @@ def bezier_curve(points, nTimes=1000):
 
     t = np.linspace(0.0, 1.0, nTimes)
 
-    polynomial_array = np.array([bernstein_poly(i, nPoints-1, t) for i in range(0, nPoints)   ])
+    polynomial_array = np.array(
+        [bernstein_poly(i, nPoints - 1, t) for i in range(0, nPoints)]
+    )
 
     input_vals = np.dot(inputPoints, polynomial_array)
     output_vals = np.dot(outputPoints, polynomial_array)
 
     return input_vals, output_vals
 
-bezier_points = [
-    (-1.206, -2),
-    (-0.845, 1.2),
-    (0.845, -1.2),
-    (1.206, 2)
-]
 
-#desmos link if anyone wants to tweak the curve: https://www.desmos.com/calculator/x0bo7r5ncj
+bezier_points = [(-1.206, -2), (-0.845, 1.2), (0.845, -1.2), (1.206, 2)]
+
+# desmos link if anyone wants to tweak the curve: https://www.desmos.com/calculator/x0bo7r5ncj
+
 
 def output_bezier(input_value):
-    input,output = bezier_curve(bezier_points)
+    input, output = bezier_curve(bezier_points)
     index = np.argmin(np.absolute(input - input_value))
-    
+
     return output[index]
--- src/drive/drive/joystick_controller.py
+++ src/drive/drive/joystick_controller.py
@@ -5,6 +5,7 @@ from geometry_msgs.msg import Twist
 
 from bezier_curve import output_bezier
 
+
 class Curve:
     """
     Class to handle different types of curves for joystick input.
@@ -27,7 +28,7 @@ class Curve:
         """
         if curve_type == "linear":
             self.curve = lambda x: x
-        elif curve_type == "Bezier":   
+        elif curve_type == "Bezier":
             self.curve = output_bezier
         else:
             rclpy.logging.get_logger("joystick_controller").warn(
