75d2c25ab55d89fff3ba32b2449820aa3ae286bc
Jetson <softlead@curover.ca>
2025-03-31 14:38:30 -0400

    WIP

--- src/navigation/launch/navigation.launch.py
+++ src/navigation/launch/navigation.launch.py
@@ -9,60 +9,69 @@ from pathlib import Path
 
 
 def generate_launch_description():
-
     # Get package directory
     pkg_navigation = get_package_share_directory("navigation")
 
-    # Declare launch arguments
-    use_sim_time = LaunchConfiguration("use_sim_time", default="false")
-    launch_transversability = LaunchConfiguration(
-        "launch_transversability", default="True"
-    )
-    launch_zed = LaunchConfiguration("launch_zed", default="True")
+    # Declare launch configurations
+    use_sim_time = LaunchConfiguration("use_sim_time")
+    launch_transversability = LaunchConfiguration("launch_transversability")
+    launch_zed = LaunchConfiguration("launch_zed")
+    zed_fuse_gps = LaunchConfiguration("zed_fuse_gps")
 
-    launch_transversability_cmd = DeclareLaunchArgument(
-        "launch_transversability",
-        description="Launch elevation mapping pkg if True",
-        default_value="True",
-    )
-    launch_zed_cmd = DeclareLaunchArgument(
-        "launch_zed", description="Launch description if True", default_value="True"
-    )
-    use_sim_time_cmd = DeclareLaunchArgument(
+    # Create LaunchDescription object
+    ld = LaunchDescription()
+
+    # Add launch arguments
+    ld.add_action(DeclareLaunchArgument(
         "use_sim_time",
-        description="Use simulation (Gazebo) clock if True",
         default_value="false",
-    )
+        description="Use simulation (Gazebo) clock if true"
+    ))
+    
+    ld.add_action(DeclareLaunchArgument(
+        "launch_transversability",
+        default_value="true",
+        description="Launch elevation mapping pkg if true"
+    ))
+    
+    ld.add_action(DeclareLaunchArgument(
+        "launch_zed",
+        default_value="true",
+        description="Launch ZED camera if true"
+    ))
+    
+    ld.add_action(DeclareLaunchArgument(
+        "zed_fuse_gps",
+        default_value="true",
+        description="Use false here if indoors, when true it waits for a single fix before giving a pose estimate."
+    ))
 
+    # Include transversability launch file
     transversability_launch_file_path = (
         Path(pkg_navigation) / "launch" / "traversability_gridmap.launch.py"
     )
-    transversability_cmd = IncludeLaunchDescription(
+    ld.add_action(IncludeLaunchDescription(
         PythonLaunchDescriptionSource(str(transversability_launch_file_path)),
         condition=IfCondition(launch_transversability),
-    )
+    ))
 
+    # Include ZED launch file
     zed_launch_file_path = Path(pkg_navigation) / "launch" / "zed.launch.py"
-    zed_cmd = IncludeLaunchDescription(
+    ld.add_action(IncludeLaunchDescription(
         PythonLaunchDescriptionSource(str(zed_launch_file_path)),
         condition=IfCondition(launch_zed),
-        launch_arguments={"use_sim_time": use_sim_time}.items(),
-    )
+        launch_arguments={
+            "use_sim_time": use_sim_time,
+            "zed_fuse_gps": zed_fuse_gps
+        }.items(),
+    ))
 
-    nav2_cmd = IncludeLaunchDescription(
+    # Include nav2 launch file
+    ld.add_action(IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(pkg_navigation, "launch", "nav2.launch.py")
         ),
         launch_arguments={"use_sim_time": use_sim_time}.items(),
-    )
+    ))
 
-    return LaunchDescription(
-        [
-            use_sim_time_cmd,
-            launch_transversability_cmd,
-            launch_zed_cmd,
-            transversability_cmd,
-            nav2_cmd,
-            zed_cmd,
-        ]
-    )
+    return ld
--- src/navigation/launch/zed.launch.py
+++ src/navigation/launch/zed.launch.py
@@ -1,7 +1,5 @@
 import os
-
 from ament_index_python.packages import get_package_share_directory
-
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
@@ -21,24 +19,45 @@ def generate_launch_description():
 
     # Launch configuration variables
     svo_path = LaunchConfiguration("svo_path")
+    zed_fuse_gps = LaunchConfiguration("zed_fuse_gps")
+    use_zed_efk = LaunchConfiguration("use_zed_efk")
+
+    # Launch argument declarations
     svo_path_launch_arg = DeclareLaunchArgument(
         "svo_path",
         default_value=TextSubstitution(text="live"),
         description="Path to an input SVO file.",
+    )
+    fuse_gps_arg = DeclareLaunchArgument(
+        "zed_fuse_gps",
+        default_value="true",
+        description="Use false here if indoors, when true it waits for a single fix before giving a pose estimate."
+    )
+    use_zed_efk_arg = DeclareLaunchArgument(
+        "use_zed_efk",
+        default_value="true",
+        description="Publishes odom and map"
     )
 
     zed_launch_arguments = {
         "ros_params_override_path": zed_override_params_filepath,
         "camera_model": "zed2i",
         "publish_urdf": "true",
-        "publish_tf": "false",
-        "publish_map_tf": "false",
+        "publish_tf": use_zed_efk,
+        "publish_map_tf": use_zed_efk,
+        "gnss_fusion_enabled": zed_fuse_gps,
         "svo_path": svo_path,
     }
 
     zed_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([zed_launch_filepath]),
+        PythonLaunchDescriptionSource(zed_launch_filepath),
         launch_arguments=zed_launch_arguments.items(),
     )
 
-    return LaunchDescription([svo_path_launch_arg, zed_launch])
+    ld = LaunchDescription()
+    ld.add_action(svo_path_launch_arg)
+    ld.add_action(fuse_gps_arg)
+    ld.add_action(use_zed_efk_arg)
+    ld.add_action(zed_launch)
+    
+    return ld
