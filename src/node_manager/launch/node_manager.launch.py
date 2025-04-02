1ac5dbba78ac12323e2577fa78693a077d18bfb0
KaliTheCatgirl <ejb0529@gmail.com>
2025-04-02 18:27:30 -0400

    Rebase off of main & reformat

--- src/node_manager/launch/node_manager.launch.py
+++ src/node_manager/launch/node_manager.launch.py
@@ -10,11 +10,13 @@ def generate_launch_description():
 
     params_file = os.path.join(config_dir, "launch_files.yaml")
 
-    return launch.LaunchDescription([
-        launch_ros.actions.Node(
-            package="node_manager",
-            executable="node_manager",
-            output="log",
-            parameters=[params_file],
-        )
-    ])
+    return launch.LaunchDescription(
+        [
+            launch_ros.actions.Node(
+                package="node_manager",
+                executable="node_manager",
+                output="log",
+                parameters=[params_file],
+            )
+        ]
+    )
