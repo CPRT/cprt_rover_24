e63584cb74d8612773bd99cf0ea96a90cd862357
ConnorN <connor.needham2015@gmail.com>
2025-04-26 15:55:58 -0400

    feat: configurable range of motion feat: display location in degrees feat: use ros2 parameters

--- src/servo_pkg/launch/usb_servo_launch.launch.py
+++ src/servo_pkg/launch/usb_servo_launch.launch.py
@@ -6,7 +6,7 @@ from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
     pkg_servo = get_package_share_directory("servo_pkg")
-    parameters_file = os.path.join(pkg_servo, "config", "servo_ranges.yaml")
+    parameters_file = os.path.join(pkg_servo, "config", "usb_controller.yaml")
 
     return launch.LaunchDescription(
         [
@@ -14,16 +14,13 @@ def generate_launch_description():
                 package="servo_pkg",
                 executable="USB_Servo",
                 name="USB_Servo_node",
-                parameters=[
-                    {"serial_port": "/dev/ttyACM0"},
-                    {"config_file": parameters_file},
-                ],
+                parameters=[parameters_file],
             ),
             # for example client
-            # launch_ros.actions.Node(
-            #    package="servo_pkg",
-            #    executable="servo_client",
-            #    name="servo_client_node",
-            # ),
+            launch_ros.actions.Node(
+                package="servo_pkg",
+                executable="servo_client",
+                name="servo_client_node",
+            ),
         ]
     )
