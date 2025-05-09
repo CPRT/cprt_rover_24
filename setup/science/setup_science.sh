14434bf75a206d0fe5214d182a83310fa7573fe8
ConnorN <connor.needham2015@gmail.com>
2025-05-09 19:10:50 -0400

    WIP

--- setup/science/setup_science.sh
+++ setup/science/setup_science.sh
@@ -2,7 +2,9 @@
 SCRIPT_DIR="$(dirname "$0")"
 CURRENT_DIR="$(pwd)"
 CURRENT_USER=$(whoami)
+VERSION="science-1.0.0"
 
+# Check if the script is being run from its own directory
 if [ "$SCRIPT_DIR" == "$CURRENT_DIR" ] || [ "$SCRIPT_DIR" == "." ]; then
     echo "The script is being run from its own directory."
 else
@@ -10,8 +12,7 @@ else
     exit 1
 fi
 
-./install_ros_humble.sh
-source ~/.bashrc
+# Install GStreamer dependencies
 sudo apt-get install -y \
         libgstreamer1.0-dev \
         libgstreamer-plugins-base1.0-dev \
@@ -25,7 +26,31 @@ sudo apt-get install -y \
         gstreamer1.0-x \
         gstreamer1.0-gl \
         gstreamer1.0-gtk3 \
-        gstreamer1.0-qt5
+        gstreamer1.0-qt5 \
+        libx264-dev \
+        libjpeg-dev \
+        gstreamer1.0-libcamera
+
+# Install Docker
+if ! command -v docker &> /dev/null; then
+    echo "Docker not found, installing Docker..."
+
+    # Update apt package list
+    sudo apt-get update
+
+    sudo apt-get install -y docker.io
+
+    # Verify Docker installation
+    sudo systemctl enable --now docker
+    sudo usermod -aG docker $CURRENT_USER
+    echo "Docker installed and configured."
+else
+    echo "Docker is already installed."
+fi
+
+# Build the Docker image for the project
+echo "Pulling Docker image..."
+docker pull cprtsoftware/cprt_rover_24:${VERSION}
 
 # Setup science service
 sudo cp start_science.service /etc/systemd/system/start_science.service
@@ -33,11 +58,9 @@ sudo sed -i "s|User=%i|User=${CURRENT_USER}|" /etc/systemd/system/start_science.
 sudo chmod 644 /etc/systemd/system/start_science.service
 sudo ln -s $PWD/start_science.sh /usr/local/bin/start_science.sh
 
-sudo tee /opt/ros/humble/cprt_setup.bash > /dev/null << EOF
-#!/bin/bash
-source $PWD/install/setup.bash
-EOF
-
+# Reload systemd, enable, and start the service
 sudo systemctl daemon-reload
 sudo systemctl enable start_science.service
 sudo systemctl start start_science.service
+
+echo "Setup completed successfully."
