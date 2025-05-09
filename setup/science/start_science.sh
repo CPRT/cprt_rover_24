888129194f885cf3fa8d36ae8fd1459f9e52e528
ConnorN <connor.needham2015@gmail.com>
2025-05-09 19:10:50 -0400

    WIP

---
+++ setup/science/start_science.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+IMAGE=cprtsoftware/cprt_rover_24
+TAG=science-latest
+JETSON_IP=192.168.0.55
+PORT=9000
+
+# Stop containers on exit
+cleanup() {
+    echo "Stopping containers..."
+    docker stop servo_container sensor_container
+    exit 0
+}
+
+trap cleanup SIGINT SIGTERM SIGHUP EXIT
+
+# Shared args
+DOCKER_ARGS="--network host --privileged -e RMW_IMPLEMENTATION=${RMW_IMPLEMENTATION} -v /dev/gpiomem:/dev/gpiomem"
+
+# Run containers in detached mode
+docker run -d --rm --name servo_container ${DOCKER_ARGS} ${IMAGE}:${TAG} \
+    ros2 launch servo_pkg pi_servo_launch.launch.py
+
+docker run -d --rm --name sensor_container ${DOCKER_ARGS} ${IMAGE}:${TAG} \
+    ros2 run science_sensors microscope_control
+
+docker run -d --rm --name sensor_container ${DOCKER_ARGS} ${IMAGE}:${TAG} \
+    ros2 launch science_sensors gas_sensor.launch.py
+
+gst-launch-1.0 libcamerasrc ! jpegenc ! jpegparse ! rtpjpegpay ! udpsink host=${JETSON_IP} port=${PORT} 
+
+wait
