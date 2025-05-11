dd28e26bf9da4ea060ed56f1fab24deba81d6741
ConnorN <connor.needham2015@gmail.com>
2025-05-11 01:05:24 -0400

    Add limit switch support

--- src/science_sensors/launch/gpio.launch.py
+++ src/science_sensors/launch/gpio.launch.py
@@ -14,5 +14,17 @@ def generate_launch_description():
                     {"gpio_pins": [11, 6]},
                 ],
             ),
+            launch_ros.actions.Node(
+                package="science_sensors",
+                executable="pi_gpio_reader",
+                name="gpio_reader_node",
+                parameters=[
+                    {"gpio_pins": [12]},
+                    {"interval": 1.0},
+                ],
+                remappings=[
+                    ("/gpio/12", "/science/ground"),
+                ],
+            ),
         ]
     )
