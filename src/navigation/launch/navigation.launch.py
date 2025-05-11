0416d800a9e4ce8366415d23def531bc9d21a291
Jetson <softlead@curover.ca>
2025-05-10 21:21:26 -0400

    Experimental nav2 lookup table changes

--- src/navigation/launch/navigation.launch.py
+++ src/navigation/launch/navigation.launch.py
@@ -3,7 +3,7 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, TextSubstitution
 from launch.conditions import IfCondition
 from pathlib import Path
 
@@ -42,11 +42,21 @@ def generate_launch_description():
         condition=IfCondition(launch_transversability),
     )
 
+    svo_path = LaunchConfiguration("svo_path")
+    svo_path_launch_arg = DeclareLaunchArgument(
+        "svo_path",
+        default_value=TextSubstitution(text="live"),
+        description="Path to an input SVO file.",
+    )
+
     zed_launch_file_path = Path(pkg_navigation) / "launch" / "zed.launch.py"
     zed_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(str(zed_launch_file_path)),
         condition=IfCondition(launch_zed),
-        launch_arguments={"use_sim_time": use_sim_time}.items(),
+        launch_arguments={
+            "use_sim_time": use_sim_time,
+            "svo_path": svo_path
+        }.items(),
     )
 
     nav2_cmd = IncludeLaunchDescription(
@@ -63,6 +73,7 @@ def generate_launch_description():
             launch_zed_cmd,
             transversability_cmd,
             nav2_cmd,
+            svo_path_launch_arg,
             zed_cmd,
         ]
     )
