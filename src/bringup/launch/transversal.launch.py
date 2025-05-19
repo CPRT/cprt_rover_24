29d717f15cc491bac95c9d24088a93f7eee53bf9
ConnorN <connor.needham2015@gmail.com>
2025-05-18 21:59:43 -0400

    arm integration

--- src/bringup/launch/transversal.launch.py
+++ src/bringup/launch/transversal.launch.py
@@ -26,7 +26,6 @@ def generate_launch_description():
         ("gps", "rover.launch.py"),
         ("joystick_control", "controller.launch.py"),
         ("navigation", "zed.launch.py"),
-        ("localization", "description.launch.py"),
         ("servo_pkg", "usb_servo_launch.launch.py"),
     ]
     return LaunchDescription(get_included_launch_descriptions(launch_files))
