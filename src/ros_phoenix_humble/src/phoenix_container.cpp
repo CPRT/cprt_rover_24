fd5db747961c63727f428e0c9e427ecfb8a712ff
Erik Caldwell <erikcaell@gmail.com>
2025-06-12 20:44:24 -0400

    talon nodes: use multi-threaded executor

--- src/ros_phoenix_humble/src/phoenix_container.cpp
+++ src/ros_phoenix_humble/src/phoenix_container.cpp
@@ -1,13 +1,14 @@
 #include <memory>
 #include <string>
 
+#include "rclcpp/executors.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_components/component_manager.hpp"
 #include "ros_phoenix/phoenix_manager.hpp"
 
 int main(int argc, char *argv[]) {
   rclcpp::init(argc, argv);
-  auto exec = std::make_shared<rclcpp::executors::SingleThreadedExecutor>();
+  auto exec = std::make_shared<rclcpp::executors::MultiThreadedExecutor>();
   auto phoenix_manager = ros_phoenix::PhoenixManager::getInstance(exec);
 
   exec->add_node(phoenix_manager);
