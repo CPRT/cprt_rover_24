e17ce88242a976983e30ce259d4cf362f5cf63af
AriaWong <nwott1@gmail.com>
2025-06-20 14:18:07 -0400

    Added presets menu

--- src/camera_streaming_qt/include/camera_streaming_qt/widgets/main_widget.h
+++ src/camera_streaming_qt/include/camera_streaming_qt/widgets/main_widget.h
@@ -1,30 +1,39 @@
 #ifndef MAIN_WIDGET_H
 #define MAIN_WIDGET_H
 
-#include <QWidget>
-#include <QVBoxLayout>
 #include <QLineEdit>
 #include <QPushButton>
+#include <QVBoxLayout>
+#include <QWidget>
 
 class MainWidget : public QWidget {
   Q_OBJECT
 
-  public:
-    MainWidget(QWidget* parent = nullptr); 
-    ~MainWidget();
+ public:
+  MainWidget(QWidget* parent = nullptr);
+  ~MainWidget();
 
-  public slots:
-    void set_signal_server_ip(QString ip);
+ public slots:
+  void set_signal_server_ip(QString ip);
 
-  private:
-    QVBoxLayout* main_layout_;
-    
-    // Signal server UI
-    QHBoxLayout* signal_server_layout_;
-    QLineEdit* server_ip_line_edit_;
-    QPushButton* server_connect_button_; 
+ private:
+  QVBoxLayout* main_layout_;
 
-    QString signal_server_ip_;
+  // Presets UI
+  QHBoxLayout* preset_layout_;
+  QPushButton* drive_preset_button_;
+  QPushButton* eef_preset_button_;
+  QPushButton* microscope_preset_button_;
+  QPushButton* belly_preset_button_;
+  QPushButton* drive_eef_preset_button_;
+  QPushButton* eef_drive_preset_button_;
+
+  // Signal server UI
+  QHBoxLayout* signal_server_layout_;
+  QLineEdit* server_ip_line_edit_;
+  QPushButton* server_connect_button_;
+
+  QString signal_server_ip_;
 };
 
-#endif 
+#endif
--- src/camera_streaming_qt/src/widgets/main_widget.cpp
+++ src/camera_streaming_qt/src/widgets/main_widget.cpp
@@ -1,9 +1,32 @@
-#include <QDebug>
 #include "widgets/main_widget.h"
 
-MainWidget::MainWidget(QWidget* parent) 
-  : QWidget(parent) {
+#include <QDebug>
+
+MainWidget::MainWidget(QWidget* parent) : QWidget(parent) {
   main_layout_ = new QVBoxLayout(this);
+
+  // Setup presets UI
+  preset_layout_ = new QHBoxLayout();
+
+  drive_preset_button_ = new QPushButton("Drive");
+  preset_layout_->addWidget(drive_preset_button_);
+
+  eef_preset_button_ = new QPushButton("EEF");
+  preset_layout_->addWidget(eef_preset_button_);
+
+  microscope_preset_button_ = new QPushButton("Microscope");
+  preset_layout_->addWidget(microscope_preset_button_);
+
+  belly_preset_button_ = new QPushButton("Belly");
+  preset_layout_->addWidget(belly_preset_button_);
+
+  drive_eef_preset_button_ = new QPushButton("Drive + EEF");
+  preset_layout_->addWidget(drive_eef_preset_button_);
+
+  eef_drive_preset_button_ = new QPushButton("EEF + Drive");
+  preset_layout_->addWidget(eef_drive_preset_button_);
+
+  main_layout_->addLayout(preset_layout_);
 
   // Setup signal server UI
   signal_server_layout_ = new QHBoxLayout();
@@ -14,14 +37,16 @@ MainWidget::MainWidget(QWidget* parent)
   server_connect_button_ = new QPushButton("Connect");
   signal_server_layout_->addWidget(server_connect_button_);
 
-  connect(server_ip_line_edit_, &QLineEdit::textChanged, this, &MainWidget::set_signal_server_ip);
+  // Make widgets start at top
+  main_layout_->setAlignment(Qt::AlignTop);
+
+  connect(server_ip_line_edit_, &QLineEdit::textChanged, this,
+          &MainWidget::set_signal_server_ip);
 
   main_layout_->addLayout(signal_server_layout_);
 }
 
-MainWidget::~MainWidget() {
-
-}
+MainWidget::~MainWidget() {}
 
 void MainWidget::set_signal_server_ip(QString ip) {
   signal_server_ip_ = ip;
