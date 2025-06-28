d69c0ecd05bef2d399cf22e630cd45c677fa7682
Jetson <softlead@curover.ca>
2025-06-28 11:57:52 -0400

    WIP

--- build.sh
+++ build.sh
@@ -24,4 +24,8 @@ done
 
 rosdep install --from-paths src -i -r -y
 
-colcon build --symlink-install --continue-on-error --cmake-args=-DCMAKE_BUILD_TYPE=Release --parallel-workers $(nproc)
+aarch=$(uname -m)
+
+colcon build --symlink-install --continue-on-error --cmake-args=-DCMAKE_BUILD_TYPE=Release --parallel-workers $(nproc) \
+              --build-base build-$aarch \
+              --install-base install-$aarch
