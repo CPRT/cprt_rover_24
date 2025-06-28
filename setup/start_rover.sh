470abba345804d82ec729fadeabd016ccf382e89
Jetson <softlead@curover.ca>
2025-06-28 11:57:52 -0400

    WIP

--- setup/start_rover.sh
+++ setup/start_rover.sh
@@ -2,4 +2,5 @@
 source ~/gstreamer/setupGstreamer.sh
 source /opt/ros/humble/setup.bash
 source /opt/ros/humble/cprt_setup.bash
+sudo enablecan.sh
 ros2 launch rosbridge_server rosbridge_websocket_launch.xml
