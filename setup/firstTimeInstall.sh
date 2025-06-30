347f614f3a5a7997d6beb061f070b8889bbd98e6
Connor Needham <129120300+ConnorNeed@users.noreply.github.com>
2025-06-30 10:56:50 -0400

    fix: restart usb ports on startup (#98)

--- setup/firstTimeInstall.sh
+++ setup/firstTimeInstall.sh
@@ -20,14 +20,24 @@ source ~/.bashrc
 source ~/.bashrc
 ./setup_service.sh
 
+# Jetson Only setup
+if [ -f /etc/nv_tegra_release ]; then
+    echo "Jetson detected, running Jetson specific setup."
+else
+    echo "Non-Jetson system detected, skipping Jetson specific setup."
+    exit 0
+fi
 sudo cp enablecan.sh /usr/local/bin || exit 1
 echo "$USER ALL=(ALL) NOPASSWD: /usr/local/bin/enablecan.sh" | sudo tee -a /etc/sudoers > /dev/null
+
 sudo cp turnOnRaman.sh /usr/local/bin
 sudo chmod 755 /usr/local/bin/turnOnRaman.sh
 
 echo "${SUDO_USER:-$USER} ALL=(ALL) NOPASSWD: /usr/local/bin/turnOnRaman.sh" \
   | sudo tee /etc/sudoers.d/turnonraman >/dev/null
 sudo chmod 0440 /etc/sudoers.d/turnonraman
+
+./usb_service_setup.sh
 
 UDEV_RULE='SUBSYSTEM=="video4linux", ATTR{name}=="Arducam B0495 (USB3 2.3MP)", ATTRS{idVendor}=="04b4", ATTR{index}=="0", ATTRS{idProduct}=="0495", SYMLINK+="drive_camera", MODE="0666"'
 echo "$UDEV_RULE" | sudo tee /etc/udev/rules.d/99-arducam.rules > /dev/null
