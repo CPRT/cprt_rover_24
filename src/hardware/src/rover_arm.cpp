48067194e6f4ac2b2dc8dce9d10ce49811b623dd
Darren W <darrenwallace@gmail.com>
2025-07-04 17:24:03 -0400

    Added TalonSRX interface for the arm and exposed joint perameters to urdf

--- src/hardware/src/rover_arm.cpp
+++ src/hardware/src/rover_arm.cpp
@@ -1,6 +1,7 @@
 #include "rover_arm.h"
 
 namespace ros2_control_rover_arm {
+
 hardware_interface::CallbackReturn RoverArmHardwareInterface::on_init(
     const hardware_interface::HardwareInfo &info) {
   if (hardware_interface::SystemInterface::on_init(info) !=
@@ -11,24 +12,33 @@ hardware_interface::CallbackReturn RoverArmHardwareInterface::on_init(
   hw_position_states_.resize(info_.joints.size(),
                              std::numeric_limits<double>::quiet_NaN());
   hw_velocity_states_.resize(info_.joints.size(), 0);
-  temp_.resize(info_.joints.size() * 2, 0);
+  control_type_.resize(info_.joints.size(), 0);
+  talon_controllers_.clear();
+  kP_.resize(info_.joints.size(), 0.0);
+  kI_.resize(info_.joints.size(), 0.0);
+  kD_.resize(info_.joints.size(), 0.0);
+  control_type_str_.resize(info_.joints.size());
+  sensor_type_.resize(info_.joints.size());
+  sensor_ticks_.resize(info_.joints.size(), 4096);
+  sensor_offset_.resize(info_.joints.size(), 0.0);
+  crossover_mode_.resize(info_.joints.size(), false);
 
   for (const hardware_interface::ComponentInfo &joint : info_.joints) {
-    std::string node_name;
+    int can_id = -1;
     for (const auto &param : joint.parameters) {
-      std::string name(param.first);
-      std::string value(param.second);
-      if (name == "node_name") {
-        node_name = value;
+      if (param.first == "can_id") {
+        can_id = std::stoi(param.second);
       }
     }
-    if (node_name.empty()) {
+    if (can_id < 0) {
       RCLCPP_FATAL(rclcpp::get_logger("RoverArmHardwareInterface"),
-                   "Missing necessary parameter node_name for joint %s",
+                   "Missing or invalid can_id for joint %s",
                    joint.name.c_str());
       return hardware_interface::CallbackReturn::ERROR;
     }
-    node_names_.push_back(node_name);
+    talon_controllers_.emplace_back(
+        std::make_shared<ctre::phoenix::motorcontrol::can::TalonSRX>(can_id));
+
     if (joint.command_interfaces.size() != 1) {
       RCLCPP_FATAL(rclcpp::get_logger("RoverArmHardwareInterface"),
                    "Joint '%s' has %zu command interface. 1 expected.",
@@ -37,10 +47,12 @@ hardware_interface::CallbackReturn RoverArmHardwareInterface::on_init(
     }
     if (joint.command_interfaces[0].name ==
         hardware_interface::HW_IF_POSITION) {
-      control_type_.emplace_back(ros_phoenix::msg::MotorControl::POSITION);
+      control_type_[talon_controllers_.size() - 1] =
+          interfaces::msg::MotorControl::POSITION;
     } else if (joint.command_interfaces[0].name ==
                hardware_interface::HW_IF_VELOCITY) {
-      control_type_.emplace_back(ros_phoenix::msg::MotorControl::VELOCITY);
+      control_type_[talon_controllers_.size() - 1] =
+          interfaces::msg::MotorControl::VELOCITY;
     } else {
       RCLCPP_FATAL(rclcpp::get_logger("RoverArmHardwareInterface"),
                    "Joint '%s' has unknown command interface %s.",
@@ -54,36 +66,39 @@ hardware_interface::CallbackReturn RoverArmHardwareInterface::on_init(
                    joint.name.c_str(), joint.state_interfaces.size());
       return hardware_interface::CallbackReturn::ERROR;
     }
-
     if (joint.state_interfaces[0].name != hardware_interface::HW_IF_POSITION) {
       RCLCPP_FATAL(rclcpp::get_logger("RoverArmHardwareInterface"),
-                   "Joint '%s' have %s state interface. '%s' expected.",
+                   "Joint '%s' has %s state interface. '%s' expected.",
                    joint.name.c_str(), joint.state_interfaces[0].name.c_str(),
                    hardware_interface::HW_IF_POSITION);
       return hardware_interface::CallbackReturn::ERROR;
     }
     if (joint.state_interfaces[1].name != hardware_interface::HW_IF_VELOCITY) {
       RCLCPP_FATAL(rclcpp::get_logger("RoverArmHardwareInterface"),
-                   "Joint '%s' have %s state interface. '%s' expected.",
+                   "Joint '%s' has %s state interface. '%s' expected.",
                    joint.name.c_str(), joint.state_interfaces[1].name.c_str(),
                    hardware_interface::HW_IF_VELOCITY);
       return hardware_interface::CallbackReturn::ERROR;
     }
   }
 
-  node_ptr_ = rclcpp::Node::make_shared("arm_hw_subscriber");
+  // Create a debug node for publishing status
+  debug_node_ = std::make_shared<rclcpp::Node>("rover_arm_debug_node");
+  status_publishers_.clear();
+  for (const auto &joint : info_.joints) {
+    std::string topic = joint.name + "/status";
+    status_publishers_.push_back(
+        debug_node_->create_publisher<interfaces::msg::MotorStatus>(topic, 10));
+  }
 
-  executor_ptr_ = std::make_shared<rclcpp::executors::SingleThreadedExecutor>();
-
-  executor_ptr_->add_node(node_ptr_);
-  executor_thread_ = std::thread([this]() { this->executor_ptr_->spin(); });
+  RCLCPP_INFO(rclcpp::get_logger("RoverArmHardwareInterface"),
+              "Successfully initialized TalonSRX controllers!");
 
   return hardware_interface::CallbackReturn::SUCCESS;
 }
 
 hardware_interface::CallbackReturn RoverArmHardwareInterface::on_configure(
     const rclcpp_lifecycle::State & /*previous_state*/) {
-  // reset values always when configuring hardware
   for (uint i = 0; i < hw_position_states_.size(); i++) {
     hw_position_states_[i] = 0;
     hw_velocity_states_[i] = 0;
@@ -91,34 +106,29 @@ hardware_interface::CallbackReturn RoverArmHardwareInterface::on_configure(
   }
   RCLCPP_INFO(rclcpp::get_logger("RoverArmHardwareInterface"),
               "Successfully configured!");
-
   return hardware_interface::CallbackReturn::SUCCESS;
 }
 
 std::vector<hardware_interface::StateInterface>
 RoverArmHardwareInterface::export_state_interfaces() {
   std::vector<hardware_interface::StateInterface> state_interfaces;
-  for (uint i = 0; i < info_.joints.size(); i++) {
+  for (uint i = 0; i < hw_position_states_.size(); i++) {
     state_interfaces.emplace_back(hardware_interface::StateInterface(
-        info_.joints[i].name, hardware_interface::HW_IF_POSITION,
+        "joint" + std::to_string(i), hardware_interface::HW_IF_POSITION,
         &hw_position_states_[i]));
-  }
-
-  for (uint i = 0; i < info_.joints.size(); i++) {
     state_interfaces.emplace_back(hardware_interface::StateInterface(
-        info_.joints[i].name, hardware_interface::HW_IF_VELOCITY,
+        "joint" + std::to_string(i), hardware_interface::HW_IF_VELOCITY,
         &hw_velocity_states_[i]));
   }
-
   return state_interfaces;
 }
 
 std::vector<hardware_interface::CommandInterface>
 RoverArmHardwareInterface::export_command_interfaces() {
   std::vector<hardware_interface::CommandInterface> command_interfaces;
-  for (uint i = 0; i < info_.joints.size(); i++) {
+  for (uint i = 0; i < hw_commands_.size(); i++) {
     command_interfaces.emplace_back(hardware_interface::CommandInterface(
-        info_.joints[i].name, phoenix_to_HW_type(control_type_[i]),
+        "joint" + std::to_string(i), phoenix_to_HW_type(control_type_[i]),
         &hw_commands_[i]));
   }
   return command_interfaces;
@@ -126,84 +136,81 @@ RoverArmHardwareInterface::export_command_interfaces() {
 
 hardware_interface::CallbackReturn RoverArmHardwareInterface::on_activate(
     const rclcpp_lifecycle::State & /*previous_state*/) {
-  // command and state should be equal when starting
-  publish_sub_ = node_ptr_->create_subscription<std_msgs::msg::Bool>(
-      "/arm_active", 10,
-      [&](const std_msgs::msg::Bool::SharedPtr msg) { publish_ = msg->data; });
-  const size_t num_joints = info_.joints.size();
-  for (size_t i = 0; i < num_joints; i++) {
+  for (size_t i = 0; i < hw_commands_.size(); i++) {
     hw_commands_[i] = hw_position_states_[i];
-    encoder_subscribers_.emplace_back(
-        node_ptr_->create_subscription<ros_phoenix::msg::MotorStatus>(
-            node_names_[i] + "/status", 10,
-            [this, i](const ros_phoenix::msg::MotorStatus::SharedPtr msg) {
-              temp_[i] = msg->position;
-            }));
-    // Create publisher for each joint
-    motor_publishers_.emplace_back(
-        node_ptr_->create_publisher<ros_phoenix::msg::MotorControl>(
-            node_names_[i] + "/set", 10));
   }
-  // Create a service to switch open loop
-  open_loop_service_ = node_ptr_->create_service<std_srvs::srv::SetBool>(
-      "open_loop",
-      [this](const std::shared_ptr<std_srvs::srv::SetBool::Request> request,
-             std::shared_ptr<std_srvs::srv::SetBool::Response> response) {
-        open_loop_ = request->data;
-        response->success = true;
-        response->message =
-            "Open loop mode set to " + std::to_string(open_loop_);
-      });
   RCLCPP_INFO(rclcpp::get_logger("RoverArmHardwareInterface"),
               "Successfully activated!");
-
   return hardware_interface::CallbackReturn::SUCCESS;
 }
 
 hardware_interface::CallbackReturn RoverArmHardwareInterface::on_deactivate(
     const rclcpp_lifecycle::State & /*previous_state*/) {
+  RCLCPP_INFO(rclcpp::get_logger("RoverArmHardwareInterface"),
+              "Successfully deactivated!");
   return hardware_interface::CallbackReturn::SUCCESS;
 }
 
 hardware_interface::return_type RoverArmHardwareInterface::read(
-    const rclcpp::Time & /*time*/, const rclcpp::Duration & /*period*/) {
-  const int num_joints = info_.joints.size();
-  if (open_loop_) {
-    for (int i = 0; i < num_joints; i++) {
-      hw_position_states_[i] = hw_commands_[i];
-      hw_velocity_states_[i] = 0;
-    }
-    return hardware_interface::return_type::OK;
-  }
-  for (int i = 0; i < num_joints; i++) {
-    hw_position_states_[i] = temp_[i];
-    hw_velocity_states_[i] = temp_[i + num_joints];
-  }
+    const rclcpp::Time &, const rclcpp::Duration &) {
+  for (size_t i = 0; i < talon_controllers_.size(); ++i) {
+    double raw_ticks = talon_controllers_[i]->GetSelectedSensorPosition();
+    double offset = sensor_offset_[i];
+    double ticks_per_rev = static_cast<double>(sensor_ticks_[i]);
+    double pos_rad = 0.0;
 
+    // Convert to radians, apply offset
+    pos_rad = ((raw_ticks / ticks_per_rev) * 2.0 * M_PI) + offset;
+
+    // Crossover mode: wrap between 0 and 2pi
+    if (crossover_mode_[i]) {
+      pos_rad = std::fmod(pos_rad, 2.0 * M_PI);
+      if (pos_rad < 0) pos_rad += 2.0 * M_PI;
+    }
+
+    hw_position_states_[i] = pos_rad;
+    hw_velocity_states_[i] =
+        talon_controllers_[i]
+            ->GetSelectedSensorVelocity();  // May want to convert this as well
+                                            // to rad/s TBD
+
+    // Publish status for debug
+    interfaces::msg::MotorStatus status_msg;
+    status_msg.temperature = talon_controllers_[i]->GetTemperature();
+    status_msg.bus_voltage = talon_controllers_[i]->GetBusVoltage();
+    status_msg.output_percent = talon_controllers_[i]->GetMotorOutputPercent();
+    status_msg.output_voltage = talon_controllers_[i]->GetMotorOutputVoltage();
+    status_msg.output_current = talon_controllers_[i]->GetOutputCurrent();
+    status_msg.position = hw_position_states_[i];
+    status_msg.velocity = hw_velocity_states_[i];
+    status_msg.fwd_limit = talon_controllers_[i]->IsFwdLimitSwitchClosed() == 1;
+    status_msg.rev_limit = talon_controllers_[i]->IsRevLimitSwitchClosed() == 1;
+
+    status_publishers_[i]->publish(status_msg);
+  }
+  rclcpp::spin_some(debug_node_);
   return hardware_interface::return_type::OK;
 }
 
 hardware_interface::return_type RoverArmHardwareInterface::write(
-    const rclcpp::Time & /*time*/, const rclcpp::Duration & /*period*/) {
-  if (!publish_) {
-    return hardware_interface::return_type::OK;
+    const rclcpp::Time &, const rclcpp::Duration &) {
+  for (size_t i = 0; i < talon_controllers_.size(); ++i) {
+    if (control_type_[i] == interfaces::msg::MotorControl::POSITION) {
+      talon_controllers_[i]->Set(
+          ctre::phoenix::motorcontrol::ControlMode::Position, hw_commands_[i]);
+    } else if (control_type_[i] == interfaces::msg::MotorControl::VELOCITY) {
+      talon_controllers_[i]->Set(
+          ctre::phoenix::motorcontrol::ControlMode::Velocity, hw_commands_[i]);
+    }
   }
-  ros_phoenix::msg::MotorControl request;
-  const int num_joints = info_.joints.size();
-  for (int i = 0; i < num_joints; i++) {
-    request.mode = control_type_[i];
-    request.value = hw_commands_[i];
-    motor_publishers_[i]->publish(request);
-  }
-
   return hardware_interface::return_type::OK;
 }
 
 std::string RoverArmHardwareInterface::phoenix_to_HW_type(
     const int control_type) {
-  if (control_type == ros_phoenix::msg::MotorControl::POSITION) {
+  if (control_type == interfaces::msg::MotorControl::POSITION) {
     return hardware_interface::HW_IF_POSITION;
-  } else if (control_type == ros_phoenix::msg::MotorControl::VELOCITY) {
+  } else if (control_type == interfaces::msg::MotorControl::VELOCITY) {
     return hardware_interface::HW_IF_VELOCITY;
   }
   RCLCPP_FATAL(rclcpp::get_logger("RoverArmHardwareInterface"),
--- src/hardware/src/rover_arm.h
+++ src/hardware/src/rover_arm.h
@@ -7,30 +7,27 @@
 #include <limits>
 #include <memory>
 #include <rclcpp/rclcpp.hpp>
-#include <stdexcept>
 #include <string>
 #include <vector>
 
-#define Phoenix_No_WPI  // remove WPI dependencies
-
+#define Phoenix_No_WPI
 #include "ctre/Phoenix.h"
 #include "hardware_interface/handle.hpp"
 #include "hardware_interface/hardware_info.hpp"
 #include "hardware_interface/system_interface.hpp"
 #include "hardware_interface/types/hardware_interface_return_values.hpp"
 #include "hardware_interface/types/hardware_interface_type_values.hpp"
-#include "msg/motor_control.hpp"
-#include "msg/motor_status.hpp"
+#include "interfaces/msg/motor_control.hpp"
+#include "interfaces/msg/motor_status.hpp"
 #include "rclcpp/macros.hpp"
-#include "rclcpp/qos.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp"
 #include "rclcpp_lifecycle/state.hpp"
-#include "rcutils/logging_macros.h"
-#include "std_msgs/msg/bool.hpp"
-#include "std_srvs/srv/set_bool.hpp"
 
-using namespace std::chrono_literals;
+// Constants for input_type parameter.
+#define ANALOG 1
+#define ABSOLUTE 2
+#define RELATIVE 3
 
 namespace ros2_control_rover_arm {
 class RoverArmHardwareInterface : public hardware_interface::SystemInterface {
@@ -74,208 +71,23 @@ class RoverArmHardwareInterface : public hardware_interface::SystemInterface {
   static std::string phoenix_to_HW_type(int control_type);
 
  private:
-  // Store the command for the simulated robot
-  std::vector<std::string> node_names_;
   std::vector<double> hw_commands_;
   std::vector<double> hw_position_states_;
   std::vector<double> hw_velocity_states_;
-  std::vector<double> temp_;
   std::vector<int> control_type_;
-
-  bool open_loop_ = false;
-  bool publish_ = true;
-
-  // handling publisher and subscriber callbacks
-  rclcpp::Node::SharedPtr node_ptr_;
-  rclcpp::Executor::SharedPtr executor_ptr_;
-  std::thread executor_thread_;
-
-  std::vector<rclcpp::Subscription<ros_phoenix::msg::MotorStatus>::SharedPtr>
-      encoder_subscribers_;
-  std::vector<rclcpp::Publisher<ros_phoenix::msg::MotorControl>::SharedPtr>
-      motor_publishers_;
-  rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr publish_sub_;
-  // Service provider for open loop
-  rclcpp::Service<std_srvs::srv::SetBool>::SharedPtr open_loop_service_;
+  std::vector<std::shared_ptr<ctre::phoenix::motorcontrol::can::TalonSRX>>
+      talon_controllers_;
+  rclcpp::Node::SharedPtr debug_node_;
+  std::vector<rclcpp::Publisher<interfaces::msg::MotorStatus>::SharedPtr>
+      status_publishers_;
+  std::vector<double> kP_, kI_, kD_;
+  std::vector<std::string> control_type_str_;
+  std::vector<std::string> sensor_type_;
+  std::vector<int> sensor_ticks_;
+  std::vector<double> sensor_offset_;
+  std::vector<bool> crossover_mode_;
 };
 
 }  // namespace ros2_control_rover_arm
-
-namespace ros_phoenix {
-
-template <class MotorController, class Configuration, class FeedbackDevice,
-          class ControlMode>
-class PhoenixNode : public BaseNode {
- public:
-  explicit PhoenixNode(const std::string &name,
-                       const NodeOptions &options = NodeOptions())
-      : BaseNode(name, options) {
-    this->controller_ =
-        std::make_shared<MotorController>(this->id_, this->interface_);
-  }
-
-  virtual ~PhoenixNode() {
-    if (this->controller_) {
-      this->controller_->Set(ControlMode::Disabled, 0.0);
-    }
-  }
-
-  virtual MotorStatus::SharedPtr status() {
-    MotorStatus::SharedPtr status =
-        std::make_shared<ros_phoenix::msg::MotorStatus>();
-
-    status->temperature = this->controller_->GetTemperature();
-    status->bus_voltage = this->controller_->GetBusVoltage();
-
-    status->output_percent = this->controller_->GetMotorOutputPercent();
-    status->output_voltage = this->controller_->GetMotorOutputVoltage();
-    status->output_current = this->get_output_current();
-
-    status->position = this->controller_->GetSelectedSensorPosition() *
-                           this->sensor_multiplier_ -
-                       this->sensor_offset_;
-    // CTRE library returns velocity in units/100ms. Multiply by 10 to get
-    // units/s.
-    status->velocity = this->controller_->GetSelectedSensorVelocity() * 10.0 *
-                       this->sensor_multiplier_;
-
-    return status;
-  }
-
-  virtual void set(MotorControl::SharedPtr control_msg) {
-    if (this->follow_id_ >= 0) {
-      return;
-    }
-
-    BaseNode::set(control_msg);
-
-    ControlMode mode = static_cast<ControlMode>(control_msg->mode);
-    if (mode == ControlMode::Velocity) {
-      // CTRE library expects velocity in units/100ms
-      this->controller_->Set(
-          mode, control_msg->value / 10.0 / this->sensor_multiplier_);
-    } else if (mode == ControlMode::Position) {
-      this->controller_->Set(mode, (this->sensor_offset_ + control_msg->value) /
-                                       this->sensor_multiplier_);
-    } else if (mode == ControlMode::PercentOutput ||
-               mode == ControlMode::Disabled) {
-      this->controller_->Set(mode, control_msg->value);
-    } else {
-      this->controller_->Set(ControlMode::Disabled, 0.0);
-      RCLCPP_WARN(this->get_logger(), "Invalid control mode: %d",
-                  static_cast<int>(mode));
-    }
-  }
-
-  virtual rcl_interfaces::msg::SetParametersResult reconfigure(
-      const std::vector<rclcpp::Parameter> &params) {
-    std::lock_guard<std::mutex> guard(this->config_mutex_);
-
-    for (auto &param : params) {
-      if (param.get_name() == Parameter::ID) {
-        this->id_ = param.as_int();
-        this->controller_ =
-            std::make_shared<MotorController>(this->id_, this->interface_);
-      } else if (param.get_name() == Parameter::INTERFACE) {
-        this->interface_ = param.as_string();
-        this->controller_ =
-            std::make_shared<MotorController>(this->id_, this->interface_);
-      }
-    }
-
-    return BaseNode::reconfigure(params);
-  }
-
- protected:
-  virtual void configure() {
-    bool warned = false;
-    while (!this->configured_) {
-      std::this_thread::sleep_for(std::chrono::seconds(1));
-      std::lock_guard<std::mutex> guard(this->config_mutex_);
-      if (this->configured_) {
-        break;  // Break out if signaled to stop while sleeping
-      }
-
-      if (this->controller_->GetFirmwareVersion() == -1) {
-        if (!warned) {
-          RCLCPP_WARN(
-              this->get_logger(),
-              "Motor controller has not been seen and cannot be configured!");
-        }
-        continue;
-      }
-      SlotConfiguration slot;
-      slot.kP = this->get_parameter("P").as_double();
-      slot.kI = this->get_parameter("I").as_double();
-      slot.kD = this->get_parameter("D").as_double();
-      slot.kF = this->get_parameter("F").as_double();
-
-      Configuration config;
-      config.slot0 = slot;
-      config.voltageCompSaturation =
-          this->get_parameter("max_voltage").as_double();
-      config.pulseWidthPeriod_EdgesPerRot =
-          this->get_parameter("edges_per_rot").as_int();
-      this->configure_current_limit(config);
-
-      ErrorCode error =
-          this->controller_->ConfigAllSettings(config, 50);  // Takes up to 50ms
-      if (error != ErrorCode::OK) {
-        if (!warned) {
-          RCLCPP_WARN(this->get_logger(),
-                      "Motor controller configuration failed!");
-          warned = true;
-        }
-        continue;
-      }
-
-      if (this->get_parameter("brake_mode").as_bool())
-        this->controller_->SetNeutralMode(NeutralMode::Brake);
-      else
-        this->controller_->SetNeutralMode(NeutralMode::Coast);
-
-      this->configure_sensor();
-      this->controller_->ConfigSelectedFeedbackCoefficient(1.0);
-
-      this->controller_->EnableVoltageCompensation(true);
-      this->controller_->SetInverted(this->get_parameter("invert").as_bool());
-      this->controller_->SetSensorPhase(
-          this->get_parameter("invert_sensor").as_bool());
-      this->controller_->SelectProfileSlot(0, 0);
-
-      if (this->follow_id_ >= 0) {
-        this->controller_->Set(ControlMode::Follower, this->follow_id_);
-      }
-
-      RCLCPP_INFO(this->get_logger(),
-                  "Successfully configured Motor Controller");
-      this->configured_ = true;
-    }
-  }
-
-  // Methods to be reimplemented by specific motor controllers
-  virtual void configure_current_limit(Configuration &config) = 0;
-  virtual void configure_sensor() = 0;
-  virtual double get_output_current() = 0;
-
-  std::shared_ptr<MotorController> controller_;
-};
-
-class TalonSRXNode
-    : public PhoenixNode<ctre::phoenix::motorcontrol::can::TalonSRX,
-                         TalonSRXConfiguration, TalonSRXFeedbackDevice,
-                         TalonSRXControlMode> {
- public:
-  TalonSRXNode(const std::string &name,
-               const NodeOptions &options = NodeOptions());
-
-  virtual ~TalonSRXNode() = default;
-
-  virtual void configure_current_limit(TalonSRXConfiguration &config);
-  virtual void configure_sensor();
-  virtual double get_output_current();
-};
-
-}  // namespace ros_phoenix
 
 #endif  // ROS2_CONTROL_ROVER_ARM
