0e651b7e4ef432f9d2bad2fc35dbf50b5fe4d373
AriaWong <nwott1@gmail.com>
2025-07-08 18:07:50 -0400

    Added gstreamer videotestsrc window

--- src/camera_streaming_qt/include/camera_streaming_qt/camera_client.h
+++ src/camera_streaming_qt/include/camera_streaming_qt/camera_client.h
@@ -1,6 +1,10 @@
 #ifndef CAMERA_CLIENT_H
 #define CAMERA_CLIENT_H
 
+#include <gst/gst.h>
+#include <gst/video/videooverlay.h>
+
+#include <QWidget>
 #include <interfaces/msg/video_source.hpp>
 #include <interfaces/srv/get_cameras.hpp>
 #include <interfaces/srv/video_out.hpp>
@@ -10,12 +14,25 @@
 
 class CameraClient : public rclcpp::Node {
  public:
+  typedef struct GstData {
+    GstElement* pipeline;
+    GstElement* source;
+    GstElement* convert;
+    GstElement* sink;
+    WId winId;
+  } GstData;
+
   CameraClient();
   ~CameraClient();
 
   std::vector<std::string> get_cameras();
   void start_video(int num_sources,
                    std::vector<interfaces::msg::VideoSource> sources);
+
+ private:
+  void open_gst_widget();
+  GstBusSyncReply bus_sync_handler(GstBus* bus, GstMessage* message,
+                                   GstData* data);
 };
 
 #endif
--- src/camera_streaming_qt/src/camera_client.cpp
+++ src/camera_streaming_qt/src/camera_client.cpp
@@ -106,6 +106,54 @@ void CameraClient::start_video(
   }
 
   RCLCPP_INFO(get_logger(), "Start video service call succeeded.");
+
+  open_gst_widget();
+}
+
+void CameraClient::open_gst_widget() {
+  GstData gst_data;
+
+  // Prepare the pipeline
+  gst_data.pipeline = gst_pipeline_new("xvoverlay");
+  gst_data.source = gst_element_factory_make("videotestsrc", NULL);
+  gst_data.sink = gst_element_factory_make("ximagesink", NULL);
+  gst_bin_add_many(GST_BIN(gst_data.pipeline), gst_data.source, gst_data.sink,
+                   NULL);
+  gst_element_link(gst_data.source, gst_data.sink);
+  gst_debug_set_active(true);
+  gst_debug_set_default_threshold(GST_LEVEL_WARNING);
+
+  // Create window where the gstreamer sink output will go to
+  QWidget window;
+  window.resize(320, 240);
+  window.show();
+  gst_data.winId = window.winId();
+
+  // For some reason X11 sink needs time before we can assign to a window (even
+  // though others don't) So need to use a bus to receive message when we can
+  // assign the sink to the window
+  // This code was found from: https://stackoverflow.com/questions/68461714/gstreamer-ximagesink-doesnt-work-when-embedded-in-gtk-window
+  GstBus* bus;
+  bus = gst_element_get_bus(gst_data.pipeline);
+  gst_bus_set_sync_handler(
+      bus, (GstBusSyncHandler)&CameraClient::bus_sync_handler, &gst_data, NULL);
+  gst_object_unref(bus);
+  gst_element_set_state(gst_data.pipeline, GST_STATE_PLAYING);
+}
+
+GstBusSyncReply CameraClient::bus_sync_handler(GstBus* bus, GstMessage* msg,
+                                               GstData* data) {
+  if (!data) return GST_BUS_PASS;
+
+  if (!gst_is_video_overlay_prepare_window_handle_message(msg)) {
+    return GST_BUS_PASS;
+  }
+
+  // Assign sink to window
+  gst_video_overlay_set_window_handle(GST_VIDEO_OVERLAY(GST_MESSAGE_SRC(msg)),
+                                      data->winId);
+
+  return GST_BUS_PASS;
 }
 
 int main(int argc, char** argv) {
@@ -114,9 +162,14 @@ int main(int argc, char** argv) {
   auto camera_client = std::make_shared<CameraClient>();
 
   QApplication a(argc, argv);
+
+  gst_init(&argc, &argv);
+
+  // Main window with all of the settings
   MainWindow* w = new MainWindow(camera_client.get(), nullptr);
   w->resize(1280, 720);
   w->show();
+
   a.exec();
 
   rclcpp::spin(camera_client);
