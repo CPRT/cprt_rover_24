6198a85df7d23ed146ef472dcea40502ae562750
Jayden <chengjayden442@gmail.com>
2025-07-23 20:20:16 -0400

    Inheritance and config for USB Servo

--- src/servo_pkg/launch/i2c_servo_launch.launch.py
+++ src/servo_pkg/launch/i2c_servo_launch.launch.py
@@ -3,12 +3,15 @@ import launch_ros.actions
 
 
 def generate_launch_description():
+    parent_params = os.path.join(pkg_servo, "config", "parent_config.yaml")
+
     return launch.LaunchDescription(
         [
             launch_ros.actions.Node(
                 package="servo_pkg",
                 executable="i2c_Servo",
                 name="USB_Servo_node",
+                parameters=[parent_params]
             ),
             launch_ros.actions.Node(
                 package="servo_pkg", executable="servo_client", name="servo_client_node"
--- src/servo_pkg/servo_pkg/USB_Servo.py
+++ src/servo_pkg/servo_pkg/USB_Servo.py
@@ -1,7 +1,10 @@
 import rclpy
 from rclpy.node import Node
 from interfaces.srv import MoveServo
+import math
+from std_msgs.msg import Float32
 from servo_pkg import maestro
+from config import Config
 
 NUM_PORTS = 12
 DEFAULT_MIN = 512
@@ -9,25 +12,20 @@ DEFAULT_MAX = 2400
 DEFAULT_MAX_DEGREES = 180
 
 
-class Servo_Info:
-    def __init__(self, port: int, min_us: int, max_us: int, max_deg: int):
-        self.port = port
-        self.min = min_us
-        self.max = max_us
-        self.rom = max_deg
 
 
-def convert_from_degrees(degrees: int, servo_info: Servo_Info) -> int:
+
+def convert_from_radians(angle: float, servo_info: Servo_Info) -> int:
     total_range = servo_info.max - servo_info.min
-    return int(servo_info.min + (total_range * degrees / servo_info.rom))
+    return int(servo_info.min + (total_range * angle * 180 / (servo_info.rom * math.pi)))
 
 
-def convert_to_degrees(value: int, servo_info: Servo_Info) -> int:
+def convert_to_radians(value: int, servo_info: Servo_Info) -> int:
     total_range = servo_info.max - servo_info.min
-    return int(servo_info.rom * (value - servo_info.min) / total_range)
+    return float(servo_info.rom * (value - servo_info.min)  * math.pi / (total_range * 180))
 
 
-class USB_Servo(Node):
+class USB_Servo(Config):
     def __init__(self):
         super().__init__("usb_servo")
 
@@ -36,86 +34,61 @@ class USB_Servo(Node):
             self.get_parameter("serial_port").get_parameter_value().string_value
         )
         self.servo = maestro.Controller(serial_port)
-        self.srv = self.create_service(MoveServo, "servo_service", self.set_position)
+        self.declare_parameter("port", 0)
+        self.port = self.get_parameter("port").get_parameter_value().integer_value
+        
+        # self.srv = self.create_service(MoveServo, "servo_service", self.set_position)
+        self.sub = self.create_subscription(Int64, , self.set_position)
 
-        self.servo_ranges = {}
         self.load_port_config()
 
-        for port, servo in self.servo_ranges.items():
+        for port, servo in self.servo_info.items():
             self.servo.setRange(port, servo.min, servo.max)
 
+
     def load_port_config(self):
-        for port_number in range(NUM_PORTS):
-            self.declare_parameter(f"port{port_number}.min", DEFAULT_MIN)
-            min_us = (
-                self.get_parameter(f"port{port_number}.min")
-                .get_parameter_value()
-                .integer_value
-            )
-            self.declare_parameter(f"port{port_number}.max", DEFAULT_MAX)
-            max_us = (
-                self.get_parameter(f"port{port_number}.max")
-                .get_parameter_value()
-                .integer_value
-            )
-            self.declare_parameter(f"port{port_number}.rom", DEFAULT_MAX_DEGREES)
-            rom = (
-                self.get_parameter(f"port{port_number}.rom")
-                .get_parameter_value()
-                .integer_value
-            )
+        self.load_config()
+        for port in range(NUM_PORTS):
+            min_us = self.get_parameter(f"servo{port}.min").get_parameter_value().double_value
+            max_us = self.get_parameter(f"servo{port}.max").get_parameter_value().double_value
+            rom = self.get_parameter(f"servo{port}.rom").get_parameter_value().double_value
+            
             # Convert microseconds to quarter-microseconds
             min_qus = min_us * 4
             max_qus = max_us * 4
 
-            self.servo_ranges[port_number] = Servo_Info(
+            self.servo_info[port_number] = Servo_Info(
                 port_number, min_qus, max_qus, rom
             )
             self.get_logger().info(
                 f"Port {port_number} -> Min: {min_qus}, Max: {max_qus}"
             )
+            self.servo.setRange(port_number, min_qus, max_qus)
 
-    def set_position(self, request: MoveServo, response: MoveServo) -> MoveServo:
-        port = request.port
-        if port not in self.servo_ranges:
-            response.status = False
-            response.status_msg = f"Invalid port: {port}"
-            return response
+    def set_position(self):
+        port = self.port
+        self.check_valid_servo(port)
 
-        servo_info = self.servo_ranges[port]
+        servo_info = self.servo_info[port]
 
-        # Update range if explicitly given
-        if (
-            request.min is not None
-            and request.min >= 0
-            and request.min < request.max
-            and request.max is not None
-            and request.max > 0
-        ):
-            servo_info.min = request.min
-            self.servo.setRange(port, servo_info.min, servo_info.max)
-            self.get_logger().info(
-                f"Updated range for port {port}: Min: {servo_info.min}, Max: {servo_info.max}"
-            )
-
-        target_value = convert_from_degrees(request.pos, servo_info)
-        current_position = convert_to_degrees(self.servo.getPosition(port), servo_info)
+        target_value = convert_from_radians(request.pos, servo_info)
+        current_position = convert_to_radians(self.servo.getPosition(port), servo_info)
 
         if not (servo_info.min <= target_value <= servo_info.max):
-            response.status = False
-            response.status_msg = f"Servo {port} input out of range.\nCurrent position: {current_position}"
+            self.get_logger().warning(
+                f"Servo {port} input out of range.\nCurrent position: {current_position}"
+            )
         else:
             self.get_logger().debug(
-                f"Received request for port {port}: {request.pos} degrees -> {target_value}"
+                f"Received request for port {port}: {request.pos} angle -> {target_value}"
             )
             self.servo.setTarget(port, target_value)
-            response.status = True
-            current_position = convert_to_degrees(
+            current_position = convert_to_radians(
                 self.servo.getPosition(port), servo_info
             )
-            response.status_msg = f"Servo {port} moved to: {current_position} degrees"
-
-        return response
+            self.get_logger().info(
+                f"Servo {port} moved to angle: {current_position}"
+            )
 
 
 def main(args=None):
